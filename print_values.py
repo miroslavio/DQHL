8b1ff42d8c1ad0bf2aea20b41a2c13a67c891b7d
miro <mm679@sussex.ac.uk>
2017-09-28 15:41:19 +0100

    Removing unnecessary/outdated files.

--- print_values.py
+++
@@ -1,8 +0,0 @@
-def print_values(data):
-    data = data['checks']
-
-    print "Time processor check results:\n", data['dqtimeproc']
-    print "Run processor check results:\n", data['dqrunproc']
-    print "Trigger processor check results:\n", data['dqtriggerproc']
-    print "PMT processor check results:\n", data['dqpmtproc']    
-    
